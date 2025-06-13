e70e7cbe310528f5f056cbd65851c0eb040bb1ab
ammarayman32 <smaynkraftyt@gmail.com>
2025-06-13 10:08:19 +0300

    theme btn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -73,7 +73,7 @@ const Navbar = () => {
         {/* Mobile nav */}
         <button
           onClick={() => setIsMenuOpen((prev) => !prev)}
-          className="md:hidden p-2 text-foreground z-50"
+          className="md:hidden mr-9 p-2 text-foreground z-50"
           aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}
         >
           {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
